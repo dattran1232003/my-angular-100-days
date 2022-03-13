e6480a5322188719704cda105a11ac7a9b444d27
dattran1232003 <dattran1232003@gmail.com>
2022-03-13 12:11:59 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  user = {
+    toString() {
+      return `Name: ${this.name}, age: ${this.age}.` 
+    },
+    name: 'Dat',
+    age: 18,
+  };
+
+  // data binding
+  
 }
